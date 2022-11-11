027e33b7aee246fd3023749a8804cbd65aa1fa72
Yossec SA <yosec.arratea22@gmail.com>
2022-11-10 22:55:20 -0500

    Creacion de la estructura de la pagina (padre - hijos)

---
+++ resources/views/tema/app.blade.php
@@ -0,0 +1,29 @@
+<!--Vista Padre --(de las vistas hijas se le puede pasar variables)-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>@yield('title')</title>
+</head>
+<body class="bg-light">
+    <div class="container shadow bg-ligth">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Tutorial YT</h1>
+            </div>
+            <div class="col-sm-12">
+                <a href="{{ route('tarea.create') }}" class="btn btn-link">Crear Nueva Tarea</a>
+                <a href="" class="btn btn-link">Listar Tareas</a>
+            </div>
+            <div class="col-sm-12">
+                <!--Esta variable llama a la vista hija-->
+                @yield('contenido')
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title>Inicio</title>
-</head>
-<body class="bg-light">
-    <div class="container shadow bg-ligth">
-        <div class="row">
-            <div class="col-sm-12">
-                <h1>Tutorial YT</h1>
-            </div>
-            <div class="col-sm-12">
-                <a href="{{ route('tarea.create') }}" class="btn btn-link">Crear Nueva Tarea</a>
-                <a href="" class="btn btn-link">Listar Tareas</a>
-            </div>
-            <div class="col-sm-12">
-                
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+@extends('tema.app')    <!-- llama a la informacion de la carpeta tema el archivo app -->
+
+@section('title', "Inicio")
+
+@section('contenido')
+    <h3>Bienvenido</h3>    
+@endsection
